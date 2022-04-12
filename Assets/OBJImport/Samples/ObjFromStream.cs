b2933a52edec6cf1574112ddd0facb3a48a41038
Vishnu Sivan <codemaker2015@gmail.com>
2022-04-12 20:01:15 +0530

    initial commit

---
+++ Assets/OBJImport/Samples/ObjFromStream.cs
@@ -0,0 +1,42 @@
+﻿using Dummiesman;
+using System.Collections;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ObjFromStream : MonoBehaviour {
+	void Start () {
+
+        StartCoroutine(GetModel("https://raw.githubusercontent.com/codemaker2015/3d-models/master/models/lamp.obj"));
+
+        //make www
+        // var www = new WWW("");
+        // while (!www.isDone)
+        //     System.Threading.Thread.Sleep(1);
+        
+        // //create stream and load
+        // var textStream = new MemoryStream(Encoding.UTF8.GetBytes(www.text));
+        // var loadedObj = new OBJLoader().Load(textStream);
+	}
+
+    IEnumerator GetModel(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Error: " + webRequest.error);
+            }
+            else
+            {
+                //create stream and load
+                var textStream = new MemoryStream(Encoding.UTF8.GetBytes(webRequest.downloadHandler.text));
+                var loadedObj = new OBJLoader().Load(textStream);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Runtime OBJ Importer
+
+This repository is a Unity demo for importing obj models at runtime. 
+
+## Demo
+
+<img src="Demos/Screenshot.png" />
